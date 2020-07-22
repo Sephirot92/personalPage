96cead696567673dce43689a7c8ca1fbd0b7d515
Łukasz Czapraga <lczapraga@gmail.com>
2020-07-23 00:44:15 +0200

    Resolved problem with servlet and 404 Error.

--- personalPage/src/com/lucas/personalpage/mvc/MainPageController.java
+++ personalPage/src/com/lucas/personalpage/mvc/MainPageController.java
@@ -1,4 +1,4 @@
-package com.lucas.personalpage;
+package com.lucas.personalpage.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
